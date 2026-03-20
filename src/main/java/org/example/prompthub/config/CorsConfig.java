425956ddd811bd58db25634b412bcfa485368468
tomirios2005 <tomirios2005@gmail.com>
2026-03-20 12:27:37 -0300

    cors update

--- src/main/java/org/example/prompthub/config/CorsConfig.java
+++ src/main/java/org/example/prompthub/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://prompt-hub-ecru.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
